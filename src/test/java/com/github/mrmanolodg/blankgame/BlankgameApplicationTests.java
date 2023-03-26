fb1a5dcc2011b2138e1707cbf4ae24c12e1c5150
MrManoloDG <frkwtf@gmail.com>
2023-03-26 01:58:33 +0100

    feat: First Commit

---
+++ src/test/java/com/github/mrmanolodg/blankgame/BlankgameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.mrmanolodg.blankgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlankgameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
